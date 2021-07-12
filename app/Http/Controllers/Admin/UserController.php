ef69a0bf6379f8afecc92cd3458ebda20435a254
root <root@PC-20210426WVZT.localdomain>
2021-07-12 15:08:34 +0800

    测试

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+//use App\Model\UserModel;
+//use Illuminate\Routing\Router;
+//use GuzzleHttp\Client;
+//use Illuminate\Support\Facades\Redis;
+
+class UserController extends Controller
+{
+    public function index(){
+        $a =1;
+        return $a;
+    }
+}
